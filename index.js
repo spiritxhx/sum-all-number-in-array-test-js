6c6435ac2964bd1f74df0cce2a0de57f02e5b24b
spiritxhx <spiritxhx@gmail.com>
2019-06-28 09:33:54 -0400

    make the test pass

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach((element) => {
+    
+    if (Array.isArray(element)) {
+      // Print out all it's items individually
+      sum += sumItems(element);
+    } else {
+      sum += element;
+    }
+  });
+  return sum;
 }
 
 module.exports = sumItems;
